0874397337550eccdb05992ae468d82e5b388b87
JULIA SOARES DE ALMEIDA <22282188@dsn127m1242315.cfp127aluno.local>
2023-08-11 16:54:13 -0300

    Button get - ok

---
+++ app/src/main/java/br/senai/sp/jandira/retrofit_reqres/BaseResponse.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.retrofit_reqres
+
+import com.google.gson.annotations.SerializedName
+
+data class BaseResponse<T>(
+
+    @SerializedName("data")
+    var data: T? = null
+)
--- app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var apiService: ApiService
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        apiService = RretrofitHelper.getInstance().create(ApiService::class.java)
+
         //Botão de Get
         findViewById<Button>(R.id.btnGet).setOnClickListener{
-            Log.e("Getting data", "Teste do botão Get")
+//          Log.e("Getting data", "Teste do botão Get")
+            getUserById()
         }
 
         //Botão de Put
@@ -32,5 +41,17 @@ class MainActivity : AppCompatActivity() {
             Log.e("Delete data", "Teste do botão Delete")
         }
 
+    }
+
+    private fun getUserById() {
+        lifecycleScope.launch {
+            val result = apiService.getUserById("1")
+
+            if(result.isSuccessful){
+                Log.e("Get", "${result.body()?.data}")
+            }else{
+                Log.e("Get error", "${result.errorBody()}")
+            }
+        }
     }
 }
